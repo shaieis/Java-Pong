4c317f9d0f95a628a2f615673a3bd993694a42cf
Shai E <shayaizi@gmail.com>
2016-10-28 15:40:28 +0300

    Shortened messages. Reversed button logic.

--- ArduinoController/ArduinoController.ino
+++ ArduinoController/ArduinoController.ino
@@ -1,37 +1,37 @@
 // DEFINES
 // Protocol names
-#define POT_NAME "POTENTIOMETER"
-#define UPPER_BUTTON_NAME "UPPER_BUTTON"
-#define LOWER_BUTTON_NAME "LOWER_BUTTON"
+#define POT_NAME "POT1"
+#define BUTTON1_NAME "BTN1"
+#define BUTTON2_NAME "BTN2"
 
 // Timers
 #define INPUT_READ_DELAY_TIME 10
 
 // Pins configuration
 const int potPin = A1; // Potentiometer's analog input pin
-const int upperBtnPin = 3; // Button's digital input pin
-const int lowerBtnPin = 2; // Button's digital input pin
+const int btn1Pin = 3; // Button's digital input pin
+const int btn2Pin = 2; // Button's digital input pin
 
 
 // VARIABLES
 // Input state
 int lastPotPos = 0;
-int lastUpperBtnState = 0;
-int lastLowerBtnState = 0;
+int lastBtn1State = 0;
+int lastBtn2State = 0;
 
 
 // FUNCTOINS
 void initLastInputsState() {
   lastPotPos = -1;
-  lastUpperBtnState = digitalRead(upperBtnPin);
-  lastLowerBtnState = digitalRead(lowerBtnPin);
+  lastBtn1State = digitalRead(btn1Pin);
+  lastBtn2State = digitalRead(btn2Pin);
 }
 
 void setup() {
   Serial.begin(9600);
   
-  pinMode(upperBtnPin, INPUT_PULLUP);
-  pinMode(lowerBtnPin, INPUT_PULLUP);
+  pinMode(btn1Pin, INPUT_PULLUP);
+  pinMode(btn2Pin, INPUT_PULLUP);
 
   initLastInputsState();
 }
@@ -39,8 +39,8 @@ void setup() {
 void loop() {
   // Read current input
   int currPotPos = analogRead(potPin);
-  int currUpperBtnState = digitalRead(upperBtnPin);
-  int currLowerBtnState = digitalRead(lowerBtnPin);
+  int currBtn1State = digitalRead(btn1Pin);
+  int currBtn2State = digitalRead(btn2Pin);
   String tempMsg = "";
 
   // Trigger events if input has changed
@@ -56,26 +56,26 @@ void loop() {
     lastPotPos = currPotPos;
   }
 
-  // Upper Button
-  if (currUpperBtnState != lastUpperBtnState)
+  // First Button
+  if (currBtn1State != lastBtn1State)
   {
-    tempMsg = UPPER_BUTTON_NAME;
+    tempMsg = BUTTON1_NAME;
     tempMsg += ":";
-    tempMsg += currUpperBtnState;
+    tempMsg += !currBtn1State; // Becuase of pull-up, logic is reversed
     
     Serial.println(tempMsg);
-    lastUpperBtnState = currUpperBtnState;
+    lastBtn1State = currBtn1State;
   }
 
-  // Lower Button
-  if (currLowerBtnState != lastLowerBtnState)
+  // Second Button
+  if (currBtn2State != lastBtn2State)
   {
-    tempMsg = LOWER_BUTTON_NAME;
+    tempMsg = BUTTON2_NAME;
     tempMsg += ":";
-    tempMsg += currLowerBtnState;
+    tempMsg += !currBtn2State; // Becuase of pull-up, logic is reversed
     
     Serial.println(tempMsg);
-    lastLowerBtnState = currLowerBtnState;
+    lastBtn2State = currBtn2State;
   }
 
   delay(INPUT_READ_DELAY_TIME);
